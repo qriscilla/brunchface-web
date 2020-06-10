65b66a03356419a22b46d5da812d966011e8c1ee
qriscilla <pri.whang@gmail.com>
2020-06-09 20:30:31 -0400

    Updates

---
+++ precache-manifest.f89257554c4574d0c7c39201436a69f4.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a59330f6a048d56326478630fe58b6",
+    "url": "/brunchface-web/index.html"
+  },
+  {
+    "revision": "7a58c1d8b4c4a2c8507e",
+    "url": "/brunchface-web/static/css/main.86456969.chunk.css"
+  },
+  {
+    "revision": "a40490a8aec44246f7a4",
+    "url": "/brunchface-web/static/js/2.bfdf3ec6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/brunchface-web/static/js/2.bfdf3ec6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a58c1d8b4c4a2c8507e",
+    "url": "/brunchface-web/static/js/main.a8d53e61.chunk.js"
+  },
+  {
+    "revision": "fb2a42aa59ea2bc5d24a",
+    "url": "/brunchface-web/static/js/runtime-main.db573ebc.js"
+  },
+  {
+    "revision": "e0203f5722548670ec302667bd3e460c",
+    "url": "/brunchface-web/static/media/polkaGrapefruit.e0203f57.png"
+  },
+  {
+    "revision": "66b1596788c94f87e64afef656a4a1d2",
+    "url": "/brunchface-web/static/media/toast.66b15967.png"
+  }
+]);
