a29f27e5833aebd2753c61ab9a1c248de0a35f3c
qriscilla <pri.whang@gmail.com>
2020-06-09 20:28:13 -0400

    Complete header

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -7,51 +7,24 @@ function Header() {
         <header className="header">
 
             <a href='/'>
-                <img src={toast} alt={toast} className='logo_img' />
-                <span className='logo_title'>BrunchFace</span>
+                <img src={toast} alt={toast} className='header_img' />
+                <span className='header_title'>BrunchFace</span>
             </a>
 
-            <input class="menu-btn" type="checkbox" id="menu-btn" />
-            <label class="menu-icon" for="menu-btn"><span class="nav-icon"></span></label>
+            <input className="menu-btn" type="checkbox" id="menu-btn" />
+            <label className="menu-icon" for="menu-btn"><span className="nav-icon"></span></label>
 
-            <ul className="menu">
-                <li><a href="/">HOME</a></li>
-                <li><a href="/">ABOUT</a></li>
-                <li><a href="/">HOW TO PLAY</a></li>
-                <li><a href="/">CONTACT</a></li>
-                <li><a href="/">TEAM</a></li>
+            <ul className="header_menu">
+                <a href="/"><li>HOME</li></a>
+                <a href="/"><li>ABOUT</li></a>
+                <a href="/"><li>HOW TO PLAY</li></a>
+                <a href="/"><li>CONTACT</li></a>
+                <a href="/"><li>TEAM</li></a>
             </ul>
 
         </header>
 
-        // <div className='header'>
-        //     <span className='header_left'>
-        //         <img src={toast} alt={toast} className='header_logo' />
-        //         BrunchFace
-        //     </span>
-        //     <span className='header_right'>
-        //         HOME &nbsp; &nbsp; &nbsp;
-        //         ABOUT &nbsp; &nbsp; &nbsp;
-        //         HOW TO PLAY &nbsp; &nbsp; &nbsp;
-        //         CONTACT &nbsp; &nbsp; &nbsp;
-        //         TEAM
-        //     </span>
-        // </div>
     )
 }
 
 export default Header;
-
-// <Grid container className='header'>
-//     <Grid item xs={3}>
-//         <img src={toast} alt={toast} className='header_logo' />
-//         BrunchFace
-//     </Grid>
-//     <Grid item xs={9} container justify='flex-end'>
-//         HOME &nbsp; &nbsp; &nbsp;
-//         ABOUT &nbsp; &nbsp; &nbsp;
-//         HOW TO PLAY &nbsp; &nbsp; &nbsp;
-//         CONTACT &nbsp; &nbsp; &nbsp;
-//         TEAM
-//     </Grid>
-// </Grid>
